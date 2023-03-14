93c85e5517aaf9cfeee4b32264419544c340e06f
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-14 18:47:15 +0200

    Add project files.

---
+++ OpenClosedPrincipleBadExam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenClosedPrincipleBadExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

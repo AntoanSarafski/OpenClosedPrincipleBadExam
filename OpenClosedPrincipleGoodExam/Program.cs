a40b73a34503e5c34dbbd82c7976f69047cfcb3f
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-14 19:42:01 +0200

    Final commit with this little example.

--- OpenClosedPrincipleGoodExam/Program.cs
+++ OpenClosedPrincipleGoodExam/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenClosedPrincipleBadExam;
+using System;
 
 namespace OpenClosedPrincipleGoodExam
 {
@@ -6,7 +7,37 @@ namespace OpenClosedPrincipleGoodExam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Store store = new Store();
+
+            store.Products.Add(new Product()
+            {
+                Name = "T-shirt",
+                Price = 100,
+                Quantity = 60
+            });
+
+            store.Products.Add(new Product()
+            {
+                Name = "Shoes",
+                Price = 200,
+                Quantity = 60
+            });
+
+            store.Products.Add(new Product()
+            {
+                Name = "Jeans",
+                Price = 400,
+                Quantity = 14
+            });
+
+            while (true)
+            {
+                Console.WriteLine("What do you want to buy?");
+                string productName = Console.ReadLine();
+
+                store.Buy(productName);
+            }
+
         }
     }
 }

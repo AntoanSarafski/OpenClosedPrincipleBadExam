73df94be50dba32b1d4a3e681ef50745975b155e
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-14 19:31:24 +0200

    Commit with logic for Sunday.

---
+++ OpenClosedPrincipleGoodExam/Promotions/IPromotion.cs
@@ -0,0 +1,12 @@
+﻿using OpenClosedPrincipleBadExam;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenClosedPrincipleGoodExam.Promotions
+{
+    public interface IPromotion
+    {
+        decimal GetPrice(Product product);
+    }
+}
---
+++ OpenClosedPrincipleGoodExam/Promotions/SundayPromotion.cs
@@ -0,0 +1,22 @@
+﻿using OpenClosedPrincipleBadExam;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenClosedPrincipleGoodExam.Promotions
+{
+    public class SundayPromotion : IPromotion
+    {
+        public decimal GetPrice(Product product)
+        {
+            decimal result = 0;
+
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                result = product.Price * 0.5m;
+                Console.WriteLine("50% off because it is Monday! Yaay!");
+            }
+            return result;
+        }
+    }
+}

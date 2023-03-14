a13ca44e9d4d2b4d5aa45821c91275161f31d153
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-14 18:51:22 +0200

    Commit with class Store and Buy method.

--- OpenClosedPrincipleBadExam/Store.cs
+++ OpenClosedPrincipleBadExam/Store.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace OpenClosedPrincipleBadExam
 {
     public class Store
     {
+        public List<Product> Products { get; set; }
 
+        public void Buy(string productName)
+        {
+            Product product = Products.FirstOrDefault(p => p.Name == productName);
+
+            if (product == null)
+            {
+                throw new ArgumentException("Product not found.")
+            }
+        }
     }
 }

dd53fc26bc6c056d989ccaf25db4d84a0cc7c0e3
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-14 18:56:18 +0200

    Commit with logic for promotions.

--- OpenClosedPrincipleBadExam/Store.cs
+++ OpenClosedPrincipleBadExam/Store.cs
@@ -7,6 +7,10 @@ namespace OpenClosedPrincipleBadExam
 {
     public class Store
     {
+        public Store()
+        {
+            Products = new List<Product>();
+        }
         public List<Product> Products { get; set; }
 
         public void Buy(string productName)
@@ -15,8 +19,41 @@ namespace OpenClosedPrincipleBadExam
 
             if (product == null)
             {
-                throw new ArgumentException("Product not found.")
+                throw new ArgumentException("Product not found.");
             }
+
+            //take money from costumer.
+
+            decimal price = product.Price;
+
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Monday)
+            {
+                price -= price * 0.2m;
+                Console.WriteLine("20% off because it is Monday! Yaay!");
+            }
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                price -= price * 0.5m;
+                Console.WriteLine("50% off because it is Monday! Yaay!");
+            }
+            if (DateTime.Now.Month == 2 && DateTime.Now.Day == 14)
+            {
+                price += price * 0.3m;
+                Console.WriteLine("30% up because it is St. Valentine! Ouch ! Why you wait until last minute ??? :( ");
+            }
+            if (DateTime.Now.Month == 12 && product.Quantity > 50)
+            {
+                price -= price * 0.5m;
+                Console.WriteLine("Final sale! Yay :)");
+            }
+
+
+            Console.WriteLine($"Buying {productName} for {price}");
+            product.Quantity -= 1;
+
+            // can input much more logic
+
+
         }
     }
 }

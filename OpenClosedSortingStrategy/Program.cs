8c975321389cd64b08cf786b2e2bbd7490309597
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-15 08:51:39 +0200

    Open/closed principle practice with reflection.

---
+++ OpenClosedSortingStrategy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenClosedSortingStrategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

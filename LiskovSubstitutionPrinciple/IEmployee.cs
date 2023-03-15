24ecbdff0933c1f10937202c95e86a4d4f63dde6
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-15 12:26:49 +0200

    Example for Liskov Principle.

---
+++ LiskovSubstitutionPrinciple/IEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiskovSubstitutionPrinciple
+{
+    public interface IEmployee
+    {
+        void Work();
+
+        void Sleep();
+
+        void GetSalary();
+    }
+}

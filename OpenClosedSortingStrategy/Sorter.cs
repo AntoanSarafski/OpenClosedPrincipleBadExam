66400d1ae13ab0f621d2f2d24f780caa5cbf0405
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-15 09:18:48 +0200

    Commit with starting use strategy pattern.

---
+++ OpenClosedSortingStrategy/Sorter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenClosedSortingStrategy
+{
+    class Sorter
+    {
+        private string strategy;
+
+        public Sorter(string strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public List<T> Sort<T>(List<T> list)
+        {
+            if (strategy == "Merge")
+            {
+                //Merge sort
+            }
+            if (strategy == "Bubble")
+            {
+                // Bubble sort
+            }
+
+            return list;
+        }
+    }
+}
---
+++ OpenClosedSortingStrategy/Strategies/ISortedStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenClosedSortingStrategy.Strategies
+{
+    public interface ISortedStrategy
+    {
+    }
+}

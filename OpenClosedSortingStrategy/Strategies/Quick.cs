be004cd8a3ab9d9c636f38146ab46029e56e4d86
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-15 09:41:55 +0200

    An example of how we can easily add a new class with new functionality without changing the existing code

---
+++ OpenClosedSortingStrategy/Strategies/Quick.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenClosedSortingStrategy.Strategies
+{
+    public class QuickSortStrategy : ISortingStrategy
+    {
+        public List<T> Sort<T>(List<T> list)
+        {
+            Console.WriteLine("Quick sorting...");
+
+            return list;
+        }
+    }
+}
